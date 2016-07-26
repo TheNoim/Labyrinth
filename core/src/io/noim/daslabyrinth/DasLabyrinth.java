a36746aeb18ab8e1075aabf6f7bee738ea09703f
Nils Bergmann <nilsbergmann@noim.io>
2016-07-26 16:22:34 +0200

    Playground

--- core/src/io/noim/daslabyrinth/DasLabyrinth.java
+++ core/src/io/noim/daslabyrinth/DasLabyrinth.java
@@ -2,7 +2,6 @@ package io.noim.daslabyrinth;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -12,6 +11,8 @@ public class DasLabyrinth extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		Functions.generateRandomeField();
+		Functions.printField();
     }
 
 	@Override
