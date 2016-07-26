ef9c7541ec7cb4e536f3b993419d7bf282f962b9
Jannis Wiehart <privat@jannismacbook.local>
2016-07-26 17:33:33 +0200

    \033[0;31mjanniswiehart updated project "Labyrinth"\033[0m

---
+++ core/src/io/noim/daslabyrinth/Settings.java
@@ -0,0 +1,10 @@
+package io.noim.daslabyrinth;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class Settings extends ApplicationAdapter {
+    @Override
+    public void create(){
+        
+    }
+}
