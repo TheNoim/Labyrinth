657f6b122755583c882697538ad0b141446007ca
Simi Simon <Simi Simon>
2016-07-26 13:19:50 +0200

    Import Fix

--- core/src/io/noim/daslabyrinth/DasLabyrinth.java
+++ core/src/io/noim/daslabyrinth/DasLabyrinth.java
@@ -2,6 +2,7 @@ package io.noim.daslabyrinth;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
