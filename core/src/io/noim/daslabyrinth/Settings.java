9e3e946a18c57d61d5f49051ff371ea83f46c8e6
Jannis Wiehart <privat@jannismacbook.local>
2016-07-26 17:36:36 +0200

    \033[0;31mjanniswiehart updated project "Labyrinth"\033[0m

--- core/src/io/noim/daslabyrinth/Settings.java
+++ core/src/io/noim/daslabyrinth/Settings.java
@@ -1,10 +1,31 @@
 package io.noim.daslabyrinth;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Settings extends ApplicationAdapter {
+    SpriteBatch batch;
+    OrthographicCamera camera;
+    Texture background;
+    Music music;
+    BitmapFont font;
+
+
     @Override
     public void create(){
-        
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        background = new Texture("");
+        music = Gdx.audio.newMusic(Gdx.files.internal("Spooky Fun.mp3"));
+        music.setLooping(true);
+        music.play();
     }
 }
