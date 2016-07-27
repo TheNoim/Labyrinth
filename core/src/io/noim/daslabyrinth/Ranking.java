eff07a394484ed3798a4a56725807e927d1f0122
heniz <flamedo008@poczta.fm>
2016-07-27 20:13:26 +0200

    FontScale

--- core/src/io/noim/daslabyrinth/Ranking.java
+++ core/src/io/noim/daslabyrinth/Ranking.java
@@ -1,4 +1,91 @@
 package io.noim.daslabyrinth;
 
-public class Ranking {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+public class Ranking implements Screen {
+
+    SpriteBatch batch;
+    public DasLabyrinth main;
+    Texture background, back;
+    Color font_color;
+    String heading;
+    Vector3 touchPosition = new Vector3();
+    OrthographicCamera camera;
+    BitmapFont font;
+
+    public Ranking(final DasLabyrinth main) {
+        create();
+        this.main = main;
+    }
+
+    public void create() {
+        heading = "RANKING";
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        background = new Texture("background.png");
+        back = new Texture("back.png");
+        font = new BitmapFont(Gdx.files.internal("Labyrinth.fnt"));
+        font_color = new Color(119, 179, 212, 1);
+        font.setColor(font_color);
+    }
+
+    private void update() {
+        if (Gdx.input.justTouched()) {
+            touchPosition.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touchPosition);
+            if (touchPosition.x >= Gdx.graphics.getWidth() / 20 - (Gdx.graphics.getWidth() / 10 / 5) &&
+                    touchPosition.x <= Gdx.graphics.getWidth() / 20 + (Gdx.graphics.getWidth() / 10 / 5 * 6) &&
+                    touchPosition.y >= (Gdx.graphics.getHeight() - Gdx.graphics.getHeight() / 30) - (back.getHeight() / 3) - (Gdx.graphics.getWidth() / 10 / 5) &&
+                    touchPosition.y <= (Gdx.graphics.getHeight() - Gdx.graphics.getHeight() / 30) - (back.getHeight() / 3) + (Gdx.graphics.getWidth() / 10 / 5 * 6)) {
+                main.setScreen(new StartMenu(main));
+            }
+        }
+    }
+
+    private void draw() {
+
+        batch.begin();
+
+        batch.draw(background, 0, 0, (float) Gdx.graphics.getWidth(), (float) Gdx.graphics.getHeight());
+        batch.draw(back, Gdx.graphics.getWidth() / 20, (Gdx.graphics.getHeight() - Gdx.graphics.getHeight() / 30) - (back.getHeight() / 3), Gdx.graphics.getWidth() / 10, Gdx.graphics.getWidth() / 10);
+        font.draw(batch, heading, Gdx.graphics.getWidth() - (Gdx.graphics.getWidth() / 20) * 17, Gdx.graphics.getHeight() - Gdx.graphics.getHeight() / 10);
+
+        batch.end();
+    }
+
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        update();
+        draw();
+    }
+
+    public void resize(int width, int height) {
+    }
+
+    public void show() {
+    }
+
+    public void hide() {
+    }
+
+    public void pause() {
+    }
+
+    public void resume() {
+    }
+
+    public void dispose() {
+        batch.dispose();
+    }
 }
--- core/src/io/noim/daslabyrinth/StartMenu.java
+++ core/src/io/noim/daslabyrinth/StartMenu.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 
@@ -27,6 +28,7 @@ public class StartMenu implements Screen {
     int ButtonWidth;
     public DasLabyrinth main;
     public Settings Settings;
+    public Ranking Ranking;
     public static Boolean playMusic, playSounds;
 
     public StartMenu(final DasLabyrinth main) {
@@ -36,6 +38,10 @@ public class StartMenu implements Screen {
 
     public StartMenu(final Settings Settings) {
         this.Settings = Settings;
+    }
+
+    public StartMenu(final Ranking Ranking) {
+        this.Ranking = Ranking;
     }
 
     public void create() {
@@ -56,7 +62,8 @@ public class StartMenu implements Screen {
             music.play();
         } else {
             music.stop();
-    }
+        }
+
 
         font = new BitmapFont(Gdx.files.internal("Labyrinth.fnt"));
         font_color = new Color(119, 179, 212, 1);
@@ -79,12 +86,26 @@ public class StartMenu implements Screen {
                 main.setScreen(new Playground(main));
             }
             if (touchPosition.x >= (Gdx.graphics.getWidth() / 2 - ButtonWidth / 2) && touchPosition.x <= (Gdx.graphics.getWidth() / 2 + ButtonWidth / 2) && touchPosition.y >= (Gdx.graphics.getHeight() / 2 - button.getHeight() / 2) && touchPosition.y <= (Gdx.graphics.getHeight() / 2 + button.getHeight() / 2)) {
-                //TODO Ranking Page
+                main.setScreen(new Ranking(main));
             }
             if (touchPosition.x >= (Gdx.graphics.getWidth() / 2 - ButtonWidth / 2) && touchPosition.x <= (Gdx.graphics.getWidth() / 2 + ButtonWidth / 2) && touchPosition.y >= (Gdx.graphics.getHeight() / 2 - button.getHeight() * 2) && touchPosition.y <= (Gdx.graphics.getHeight() / 2 - button.getHeight())) {
                 main.setScreen(new Settings(main));
             }
         }
+    }
+
+    public static float textWidth(String text, BitmapFont font){
+        GlyphLayout layout = new GlyphLayout(); //dont do this every frame! Store it as member
+        layout.setText(font, text);
+        float width = layout.width;// contains the width of the current set text
+        //float height = layout.height; // contains the height of the current set text
+        return width;
+    }
+
+    public static float scaleText(String text, BitmapFont font, int targetWidth) {
+        float textWidth = textWidth(text, font);
+        float proportion = targetWidth / textWidth;
+        return proportion;
     }
 
     private void draw() {
@@ -96,7 +117,7 @@ public class StartMenu implements Screen {
         batch.draw(button, Gdx.graphics.getWidth() / 2 - ButtonWidth / 2, Gdx.graphics.getHeight() / 2 - button.getHeight() / 2, ButtonWidth, button.getHeight());
         batch.draw(button, Gdx.graphics.getWidth() / 2 - ButtonWidth / 2, Gdx.graphics.getHeight() / 2 - button.getHeight() * 2, ButtonWidth, button.getHeight());
 
-        //font.getData().setScale(ButtonWidth * (float) 0.7, 1);
+        font.getData().setScale(scaleText(settings, font, ButtonWidth-20));
         font.draw(batch, heading, 0, Gdx.graphics.getHeight() - Gdx.graphics.getHeight() / 10, Gdx.graphics.getWidth(), 1, false);
         font.draw(batch, play, (Gdx.graphics.getWidth() / 2 - ButtonWidth / 2), Gdx.graphics.getHeight() / 2 + 2 * button.getHeight() - ((button.getHeight() - 46) / 2), ButtonWidth, 1, false);
         font.draw(batch, ranking, (Gdx.graphics.getWidth() / 2 - ButtonWidth / 2), Gdx.graphics.getHeight() / 2 + (button.getHeight() / 2) - ((button.getHeight() - 46) / 2), ButtonWidth, 1, false);
