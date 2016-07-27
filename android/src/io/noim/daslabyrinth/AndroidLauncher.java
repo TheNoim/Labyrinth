7f0b43e5335730637bf37f3b189aba37aad75236
Nils Bergmann <nilsbergmann@noim.io>
2016-07-27 09:37:52 +0200

    Fix 2

--- android/src/io/noim/daslabyrinth/AndroidLauncher.java
+++ android/src/io/noim/daslabyrinth/AndroidLauncher.java
@@ -10,18 +10,6 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-<<<<<<< HEAD
 		initialize(new Playground(), config);
-=======
-<<<<<<< HEAD
-		initialize(new Settings(), config);
-=======
-<<<<<<< HEAD
-		initialize(new Playground(), config);
-=======
-		initialize(new StartMenu(), config);
->>>>>>> db696dbce3dd7690caf9af0c323b413bb84c5d5a
->>>>>>> 0f4aa827734d85f05e2da2edf2b24bd43ffe1944
->>>>>>> bbfffb93b7ff33c09228e9b9ea70f20324bedc7f
 	}
 }
