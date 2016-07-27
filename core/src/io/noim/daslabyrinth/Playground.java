0b4c8d6d16c29fa7a36e268e3b3ac02b142cca69
Nils Bergmann <nilsbergmann@noim.io>
2016-07-27 10:51:44 +0200

    New Field

--- core/src/io/noim/daslabyrinth/Playground.java
+++ core/src/io/noim/daslabyrinth/Playground.java
@@ -1,11 +1,14 @@
 package io.noim.daslabyrinth;
 
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.Array;
 
@@ -14,9 +17,8 @@ public class Playground implements Screen {
     public DasLabyrinth main;
 
     public Playground(final DasLabyrinth main){
-        create();
+        Functions.generateRandomeField();
         this.main = main;
-
     }
 
     Array<GameField> gameFields = new Array<GameField>();
@@ -30,12 +32,36 @@ public class Playground implements Screen {
     int halffinalprozent;
     ShapeRenderer shaper;
     OrthographicCamera camera;
+    Texture background;
+    GameField newgf;
 
     public void create() {
         Functions.generateRandomeField();
-        Functions.printField();
+        int tcount = 0;
+        for (GameField gff : Functions.gameFields){
+            if (gff.isTreasure){
+                tcount++;
+            }
+        }
+        if (tcount < Functions.treasurcountermax){
+            int rnd3 = Functions.randomWithRange(0, 100);
+            if (rnd3 < 20){
+                newgf = new GameField(Functions.getTextureByType(Functions.randomWithRange(0, 3)), true, 0, 0, Functions.gameFields.size + 1, Functions.randomWithRange(0, 3), Functions.randomWithRange(0, 3));
+                int rnd2 = Functions.randomWithRange(0, 100);
+                if (rnd2 > 20){
+                    newgf.treasure = new Treasure(Functions.treasure_min, 3);
+                } else {
+                    newgf.treasure = new Treasure(Functions.treasure_max, 5);
+                }
+            } else {
+                newgf = new GameField(Functions.getTextureByType(Functions.randomWithRange(0, 3)), false, 0, 0, Functions.gameFields.size + 1, Functions.randomWithRange(0, 3), Functions.randomWithRange(0, 3));
+            }
+        } else {
+            newgf = new GameField(Functions.getTextureByType(Functions.randomWithRange(0, 3)), false, 0, 0, Functions.gameFields.size + 1, Functions.randomWithRange(0, 3), Functions.randomWithRange(0, 3));
+        }
         screen_width = Gdx.graphics.getHeight();
         screen_hight = Gdx.graphics.getWidth();
+        background = new Texture("background.png");
         batch = new SpriteBatch();
         shaper = new ShapeRenderer();
         camera = new OrthographicCamera();
@@ -60,16 +86,21 @@ public class Playground implements Screen {
         heightandwidthperfield = availablehight / 4;
         int usedwidth = heightandwidthperfield * 5;
         int q = screen_width - usedwidth;
-        startx = (int) Math.round(q / 1.2);
+        startx = (int) Math.round(q / 1.07);
     }
 
     public void draw() {
         camera.update();
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
+        batch.draw(background, 0, 0, screen_hight, screen_width);
         int x = 1;
         int xx = startx;
         int yy = halffinalprozent;
+        if (newgf != null){
+            int dd = (int) Math.round(heightandwidthperfield * 1.5);
+            batch.draw(newgf.fieldTextureRegion, yy, xx - dd, heightandwidthperfield, heightandwidthperfield);
+        }
         for (GameField gf : gameFields) {
             if (gf.x == x && gf.y <= 5) {
                 batch.draw(gf.fieldTextureRegion, yy, xx, heightandwidthperfield, heightandwidthperfield);
