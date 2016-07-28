f27f63e7a600a1ee8ad7ba3defd7a8d800c499a6
Jannis Wiehart <privat@jannismacbook.local>
2016-07-28 17:25:05 +0200

    janniswiehart updated project "Labyrinth"

--- core/src/io/noim/daslabyrinth/Settings.java
+++ core/src/io/noim/daslabyrinth/Settings.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Timer;
 
 
 public class Settings implements Screen {
